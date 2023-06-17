b0c51b539a7bdc01d8b7747b239133940233fddd
Simphiwe-Sithebe <www.simz88@gmail.com>
2023-06-17 13:14:51 -0700

    perms

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void printHello() {
+    printf("hello\n");
+
+int main(void)
+{
+	printHello();
+        return (0);
+}
