09cb585521a7d4992f45c7ae0639c4f148eab7de
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 00:56:41 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-
+#include <stdlib.h>
+/**
+*main - prints the alphabet
+*
+*Return: always 0. 
+*/
+int main(void)
+{
+char alp[26]="abcdefghijklmnopqrstuvwxyz";
+int i;
+for(i=0;i<26;i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0):
+}
