b5b29ff95125d3d528bfad85bee24bf85000c3fa
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 22:36:36 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints out alphabet in lower case 10 times.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int x;
+for (i = 0; i < 10; i++)
+{
+for ( x = 97; x <= 122; x++)
+{
+_putchar(x);
+}
+}
+_putchar('\n');
+}
