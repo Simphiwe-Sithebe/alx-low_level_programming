ccea48af7699fbdcf43892eb99f71aed12943600
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 18:38:34 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
+
 /**
-*main - Prints dif combos of two numbers
-*
-*Return: Always 0.
-*/
+ * main - Prints different combinations of two numbers
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-int k, m, n;
-for (n = 48; n < 58; n++)
-{
-for (m = 49; m < 58; m++)
-{
-if (k > m && m > n)
-putchar(n);
-putchar(m);
-putchar(k);
-if (n != 55 || m != 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+    int k, m, n;
+    
+    for (n = 48; n < 58; n++)
+    {
+        for (m = 49; m < 58; m++)
+        {
+            for (k = 50; k < 58; k++)
+            {
+                if (k > m && m > n)
+                {
+                    putchar(n);
+                    putchar(m);
+                    putchar(k);
+                    
+                    if (n != 55 || m != 56 || k != 57)
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+    
+    putchar('\n');
+    return (0);
 }
