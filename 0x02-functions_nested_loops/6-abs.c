85960e81e29dd0f0f32de8e01a872ec44fa29851
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-21 00:00:23 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs - computes absolute value
+* of integer.
+*
+* @n: input number as int.
+*
+* Return: absolute value.
+*/
+int _abs(int)
+{
+if (n >= 0)
+{
+return (n); 
+}
+else
+{
+return (n * -1);
+}
+}  
