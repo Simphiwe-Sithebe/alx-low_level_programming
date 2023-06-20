78485cb7d87b25f06180a8e728d1b2dd5160dc03
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 03:20:45 +0200

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Prints dif combos of two numbers
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+int n, m;
+for (n = 48; n <= 56; n++)
+}
+for (m = 49; m <= 57; m++)
+{
+if (m > n)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
