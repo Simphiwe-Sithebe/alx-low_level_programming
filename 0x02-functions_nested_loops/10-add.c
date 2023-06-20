c9dda3606b2c30237dfae316e356d385f3510396
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-21 01:20:52 +0200

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - Adds two value
+* @n1: The first number to be computed
+* @n2: The second number to be computed
+*Return: Integer value
+*/
+int add(int, int)
+{
+return (n1 + n2);
+}
