2075f55a594b14cd3133d726fc57a5fde94e6250
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 18:39:45 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,30 +7,29 @@
  */
 int main(void)
 {
-    int k, m, n;
+int k, m, n;
+for (n = 48; n < 58; n++)
+{
+for (m = 49; m < 58; m++)
+{
+for (k = 50; k < 58; k++)
+{
+if (k > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(k);
+
+if (n != 55 || m != 56 || k != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
     
-    for (n = 48; n < 58; n++)
-    {
-        for (m = 49; m < 58; m++)
-        {
-            for (k = 50; k < 58; k++)
-            {
-                if (k > m && m > n)
-                {
-                    putchar(n);
-                    putchar(m);
-                    putchar(k);
-                    
-                    if (n != 55 || m != 56 || k != 57)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-    
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
