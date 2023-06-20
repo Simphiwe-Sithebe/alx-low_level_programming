3e9dee937d5d0cc01760fa96c2c67f7d6c78be05
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 22:47:19 +0200

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* int _islower - Shows 1 if the input is lower case else shows 0.
+*
+* returns: 0 or 1
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
