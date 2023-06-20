e3278cb945006acfd9e83fc7f73286f255d48c8b
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-21 00:09:18 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_last_digit - Prints the last digit
+* of a number.
+* 
+* @n: Input number as integer.
+*
+* Return: Last digit.
+*/
+int print_last_digit(int n)
+{
+int l;
+l = n % 10;
+if ( n < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l); 
+}
+}
