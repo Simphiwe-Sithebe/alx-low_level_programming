ef8c3bf7c3b32efa134ba1972433169adf6196cf
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 23:28:26 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,5 +5,25 @@
 *
 * @x: The input numer as an integer.
 *
-* Re ler than zero. Zero is 0.
-* 
+* Return: 1 is greater than zero. Zero is 0.
+* -1 is less than 0.
+*/
+int print_sign(int x)
+{
+if (n > 0)
+{
+ _putchar(43);
+ return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+_putchar('\n')
+}
