9e2e81d23c469387875a847b657e69a281572512
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 21:57:38 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-int main(void) {
-printf("_putchar\n");
-return 0;
+/**
+*main - Enttry point
+*Description: prints putchar to stdout
+*Return: 0
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
 }
