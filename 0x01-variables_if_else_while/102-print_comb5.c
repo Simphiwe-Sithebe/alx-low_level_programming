de6f95f719ecb45defcbd5266390834eac3af72b
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-20 03:34:25 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,31 @@
-
+#include <stdio.h>
+/**
+*main - Prints dif combos of two numbers
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 56; i++)
+}
+for (j = 49; j <= 57; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
