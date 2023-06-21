13b73ac7e4421dfa0979f88cd031ac65323cebfd
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-21 02:04:36 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ for (; n <= 98; n++)
 if (n==98)
 {
 printf("%d", n);
-printf('\n');
+printf("\n");
 break;
 }
 else
@@ -30,7 +30,7 @@ for (; n >= 98; n--)
 if (n==98)
 {
 printf("%d", n);
-printf('\n');
+printf("\n");
 break;
 }
 else
