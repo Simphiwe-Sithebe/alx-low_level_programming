c2c2850031f99654266811f9ab97586054edb0e7
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-06-26 22:46:25 +0200

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - returns the lenght of a string.
+* @s: string
+* Return: empty output
+*/
+int _strlen(char *s)
+{
+int longi = 0;
+while (*s != '/0')
+{
+longi ++;
+s++;
+}
+}
